acdf5f67f23a10fab45233a5ca051f30b13fca5a
Ramon <ramonfrombr@gmail.com>
2024-02-18 01:01:54 -0300

    Selecionando elementos

---
+++ script.js
@@ -0,0 +1,15 @@
+const balanco = document.getElementById("balanco");
+const dinheiro_mais = document.getElementById("dinheiro-mais");
+const dinheiro_menos = document.getElementById("dinheiro-menos");
+const lista = document.getElementById("lista");
+const formulario = document.getElementById("formulario");
+const texto = document.getElementById("texto");
+const quantidade = document.getElementById("quantidade");
+
+console.log(balanco);
+console.log(dinheiro_mais);
+console.log(dinheiro_menos);
+console.log(lista);
+console.log(formulario);
+console.log(texto);
+console.log(quantidade);
